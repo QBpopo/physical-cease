001d5cf39ea3a2af2b8b796092a61a9a8fe9d58d
QBpopo <2461318613@qq.com>
2026-05-27 16:06:18 +0800

    init config

---
+++ oxfmt.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+	arrowParens: "avoid",
+	bracketSpacing: true,
+	endOfLine: "crlf",
+	insertFinalNewline: true,
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+});

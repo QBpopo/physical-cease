d33c6097302b6cdf735d2cd8b08731e5ef37a27c
QBpopo <2461318613@qq.com>
2026-05-28 01:13:37 +0800

    add entities

---
+++ src/entities.ts
@@ -0,0 +1,86 @@
+import { Position } from "./vector.ts";
+import type { DataFields } from "./types.ts";
+
+import type { FromFor, ImplFrom } from "./trait/from.ts";
+import { use_trait_from } from "./trait/from.ts";
+
+export const enum Direction {
+	Up,
+	Right,
+	Down,
+	Left,
+}
+
+// 恢复系数 e (Coefficient of Restitution)
+export class Cor {
+	top = 1;
+	right = 1;
+	bottom = 1;
+	left = 1;
+
+	constructor(cor?: DataFields<Cor>) {
+		if (!cor) return;
+		this.top = cor.top;
+		this.right = cor.right;
+		this.bottom = cor.bottom;
+		this.left = cor.left;
+	}
+
+	declare static impl_from: ImplFrom;
+	declare static from: FromFor<Cor>;
+}
+use_trait_from(Cor);
+Cor.impl_from(Number, v => new Cor({ top: v, right: v, bottom: v, left: v }));
+
+export class Block {
+	position = new Position();
+	cor = new Cor();
+	facing_dir = Direction.Up;
+	current_kinetic_energy = 0;
+
+	constructor(block?: DataFields<Block>) {
+		if (!block) return;
+		this.position = block.position;
+		this.cor = block.cor;
+		this.facing_dir = block.facing_dir;
+		this.current_kinetic_energy = block.current_kinetic_energy;
+	}
+}
+
+export class Ground {
+	position = new Position();
+	kinetic_energy_delta = 0;
+
+	constructor(ground?: DataFields<Ground>) {
+		if (!ground) return;
+		this.position = ground.position;
+		this.kinetic_energy_delta = ground.kinetic_energy_delta;
+	}
+}
+
+export class StaticBlock {
+	position = new Position();
+	cor = Cor.from(Number, 0);
+
+	constructor(static_block?: DataFields<StaticBlock>) {
+		if (!static_block) return;
+		this.position = static_block.position;
+		this.cor = static_block.cor;
+	}
+}
+
+export class Key {
+	position = new Position();
+	constructor(key?: DataFields<Key>) {
+		if (!key) return;
+		this.position = key.position;
+	}
+}
+
+export class EndZone {
+	position = new Position();
+	constructor(end_zone?: DataFields<EndZone>) {
+		if (!end_zone) return;
+		this.position = end_zone.position;
+	}
+}

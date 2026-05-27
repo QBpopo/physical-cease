bde9e1a4a819dbceeb3ff5bf0635a6cb0c61b13c
QBpopo <2461318613@qq.com>
2026-05-28 03:27:20 +0800

    add controller

---
+++ src/controller.ts
@@ -0,0 +1,59 @@
+export const enum State {
+	None,
+	Turn,
+	GainKE, // gain kinetic energy
+	ResetLevel,
+}
+
+type Timer = ReturnType<typeof setTimeout>;
+
+export type Action = {
+	turn: () => void;
+	gain_ke: () => void;
+	reset_level: () => void;
+};
+
+// 拓展性不是一般的差
+export class Controller {
+	private state = State.None;
+	private timer: Timer | null = null;
+
+	private turn = () => {};
+	private gain_ke = () => {};
+	private reset_level = () => {};
+
+	constructor(private action: Action) {}
+
+	private clear_timer() {
+		if (this.timer) {
+			clearTimeout(this.timer);
+			this.timer = null;
+		}
+	}
+
+	press() {
+		this.clear_timer();
+
+		this.state = State.Turn;
+		this.timer = setTimeout(() => {
+			this.state = State.GainKE;
+			this.timer = setTimeout(() => {
+				this.state = State.ResetLevel;
+				this.reset_level();
+				this.timer = null;
+			}, 2800);
+		}, 200);
+	}
+
+	release() {
+		this.clear_timer();
+
+		if (this.state === State.Turn) {
+			this.turn();
+		} else if (this.state === State.GainKE) {
+			this.gain_ke();
+		}
+
+		this.state = State.None;
+	}
+}

b01a6f139e3aa32cc5f1f68e9b5616498233fef2
QBpopo <2461318613@qq.com>
2026-05-29 00:29:13 +0800

    Enhanced trait `From`

--- src/trait/from.ts
+++ src/trait/from.ts
@@ -1,11 +1,10 @@
-import type { StrictConstructor } from "../types.ts";
+import type { StrictConstructor, Unwrap } from "../types.ts";
 
 export const from: unique symbol = Symbol("from");
 export const impl_from: unique symbol = Symbol("impl_from");
 
 type Self<T> = StrictConstructor<T>;
 type Source<S> = StrictConstructor<S>;
-type FromFn<T, S> = (source: InstanceType<Source<S>>) => InstanceType<Source<T>>;
 
 const conflict_error = <T, S>(self: Self<T>, source: Source<S>): never => {
 	throw new Error(`conflicting implementations of trait \`from<${source.name}>\` for type \`${self.name}\``);
@@ -15,28 +14,48 @@ const not_implemented_error = <T, S>(self: Self<T>, source: Source<S>): never =>
 	throw new Error(`the trait \`From<${source.name}>\` is not implemented for \`${self.name}\``);
 };
 
-export type ImplFrom = <S, T>(source: Source<S>, fn: FromFn<T, S>) => void;
-export type ImplFromFor<T> = <S>(source: Source<S>, value: S) => T;
+export type ImplFrom = <S, T, Arg extends Unwrap<S> = Unwrap<S>>(source: Source<S>, fn: (source: Arg) => T) => void;
+
+type _ImplFromFor<T> = <S>(value: Unwrap<S>) => T;
+type __ImplFromFor<T> = <S>(source: Source<S>, value: Unwrap<S>) => T;
+export type ImplFromFor<T> = _ImplFromFor<T> & __ImplFromFor<T>;
 
 const trait_from = new WeakMap<StrictConstructor, WeakMap<StrictConstructor, Function>>();
 
 export const use_trait_from_with_symbol = <T>(self: Self<T>) => {
 	const impls = trait_from.get(self) ?? trait_from.set(self, new WeakMap()).get(self)!;
 
+	const impl_from_fn: ImplFrom = (source, fn) => {
+		if (impls.has(source)) {
+			conflict_error(self, source);
+		}
+		impls.set(source, fn);
+	};
+
+	const _from_fn: _ImplFromFor<T> = value => {
+		const source = Object.getPrototypeOf(value).constructor;
+		return __from_fn(source, value);
+	};
+
+	const __from_fn: __ImplFromFor<T> = (source, value) => {
+		const fn = impls.get(source);
+		if (!fn) {
+			return not_implemented_error(self, source);
+		}
+		return fn(value);
+	};
+
+	const from_fn: __ImplFromFor<T> = (source, value) => {
+		if (value === undefined) {
+			return _from_fn(source);
+		} else {
+			return __from_fn(source, value);
+		}
+	};
+
 	const _self = Object.assign(self, {
-		[impl_from]: <S>(source: Source<S>, fn: FromFn<T, S>): void => {
-			if (impls.has(source)) {
-				conflict_error(self, source);
-			}
-			impls.set(source, fn);
-		},
-		[from]: <S>(source: Source<S>, value: S): T => {
-			const fn = impls.get(source);
-			if (!fn) {
-				return not_implemented_error(self, source);
-			}
-			return fn(value);
-		},
+		[impl_from]: impl_from_fn,
+		[from]: from_fn,
 	});
 
 	_self[impl_from](self, v => v);
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,13 @@ export type StrictFunction<T = unknown> = (...args: never) => T;
 export type StrictConstructor<T = unknown> = new (...args: never) => T;
 
 export type DataFields<T> = Pick<T, { [K in keyof T]: T[K] extends StrictFunction ? never : K }[keyof T]>;
+
+/* oxlint-disable typescript/no-wrapper-object-types */
+type UnwrapNumber<T> = T extends Number ? number : T;
+type UnwrapString<T> = T extends String ? string : T;
+type UnwrapBoolean<T> = T extends Boolean ? boolean : T;
+type UnwrapSymbol<T> = T extends Symbol ? symbol : T;
+type UnwrapBigInt<T> = T extends BigInt ? bigint : T;
+/* oxlint-enable typescript/no-wrapper-object-types */
+
+export type Unwrap<T> = UnwrapNumber<UnwrapString<UnwrapBoolean<UnwrapSymbol<UnwrapBigInt<T>>>>>;

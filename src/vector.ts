65cb657c518b3715f27c2f21930e3d2a4d82fd16
QBpopo <2461318613@qq.com>
2026-06-07 21:01:53 +0800

    impl `From` for `Position` impl `From` for `Velocity`

--- src/vector.ts
+++ src/vector.ts
@@ -1,4 +1,6 @@
 import type { DataFields } from "./types.ts";
+import type { FnFrom, ImplFrom } from "./trait/from.ts";
+import { use_trait_from } from "./trait/from.ts";
 
 export class Vector {
 	x: number = 0;
@@ -15,6 +17,16 @@ export class Vector {
 	}
 }
 
-export class Position extends Vector {}
+export class Position extends Vector {
+	declare static impl_From: ImplFrom;
+	declare static from: FnFrom<Position>;
+}
+use_trait_from(Position);
+Position.impl_From(Array, (v: [number, number]) => new Position({ x: v[0], y: v[1] }));
 
-export class Velocity extends Vector {}
+export class Velocity extends Vector {
+	declare static impl_From: ImplFrom;
+	declare static from: FnFrom<Velocity>;
+}
+use_trait_from(Velocity);
+Velocity.impl_From(Array, (v: [number, number]) => new Velocity({ x: v[0], y: v[1] }));

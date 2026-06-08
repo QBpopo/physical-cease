552298cf85ee03e1a1941afd6268a1b6f8a81813
QBpopo <2461318613@qq.com>
2026-06-08 16:00:01 +0800

    use direct array index instead of find for ground delta access

--- src/level/elastic.ts
+++ src/level/elastic.ts
@@ -3,13 +3,16 @@ import { Block, Ground, StaticBlock, Key, EndZone } from "../entities.ts";
 
 import { Model, ModelState } from "../model.ts";
 
-const grounds: Ground[] = Array.from({ length: 8 }, (_, i) => i + 1).flatMap(x =>
+const grounds = Array.from({ length: 8 }, (_, i) => i + 1).flatMap(x =>
 	Array.from({ length: 7 }, (_, i) => i + 1).map(
 		y => new Ground({ position: Position.from([x, y]), kinetic_energy_delta: 0 }),
 	),
 );
-grounds.find(g => g.position.eq(Position.from([5, 4])))!.kinetic_energy_delta = -1;
-grounds.find(g => g.position.eq(Position.from([6, 5])))!.kinetic_energy_delta = -1;
+
+const index = (x: number, y: number): number => (x - 1) * 7 + (y - 1);
+
+grounds[index(5, 4)]!.kinetic_energy_delta = -1;
+grounds[index(6, 5)]!.kinetic_energy_delta = -1;
 
 const static_blocks: StaticBlock[] = [
 	new StaticBlock({ position: Position.from([1, 5]), cor: Cor.from(1) }),
--- src/level/friction_ground.ts
+++ src/level/friction_ground.ts
@@ -3,23 +3,26 @@ import { Block, Ground, StaticBlock, Key, EndZone } from "../entities.ts";
 
 import { Model, ModelState } from "../model.ts";
 
-const grounds: Ground[] = Array.from({ length: 8 }, (_, i) => i + 1).flatMap(x =>
+const grounds = Array.from({ length: 8 }, (_, i) => i + 1).flatMap(x =>
 	Array.from({ length: 7 }, (_, i) => i + 1).map(
 		y => new Ground({ position: Position.from([x, y]), kinetic_energy_delta: 0 }),
 	),
 );
-grounds.find(g => g.position.eq(Position.from([2, 2])))!.kinetic_energy_delta = -2;
-grounds.find(g => g.position.eq(Position.from([2, 4])))!.kinetic_energy_delta = -1;
-grounds.find(g => g.position.eq(Position.from([3, 4])))!.kinetic_energy_delta = -1;
-grounds.find(g => g.position.eq(Position.from([3, 6])))!.kinetic_energy_delta = -1;
-grounds.find(g => g.position.eq(Position.from([4, 6])))!.kinetic_energy_delta = -1;
-grounds.find(g => g.position.eq(Position.from([6, 2])))!.kinetic_energy_delta = -2;
-grounds.find(g => g.position.eq(Position.from([6, 4])))!.kinetic_energy_delta = -1;
-grounds.find(g => g.position.eq(Position.from([6, 7])))!.kinetic_energy_delta = -1;
-grounds.find(g => g.position.eq(Position.from([7, 4])))!.kinetic_energy_delta = -1;
-grounds.find(g => g.position.eq(Position.from([7, 6])))!.kinetic_energy_delta = -1;
-grounds.find(g => g.position.eq(Position.from([8, 2])))!.kinetic_energy_delta = -1;
-grounds.find(g => g.position.eq(Position.from([8, 7])))!.kinetic_energy_delta = -3;
+
+const index = (x: number, y: number): number => (x - 1) * 7 + (y - 1);
+
+grounds[index(2, 2)]!.kinetic_energy_delta = -2;
+grounds[index(2, 4)]!.kinetic_energy_delta = -1;
+grounds[index(3, 4)]!.kinetic_energy_delta = -1;
+grounds[index(3, 6)]!.kinetic_energy_delta = -1;
+grounds[index(4, 6)]!.kinetic_energy_delta = -1;
+grounds[index(6, 2)]!.kinetic_energy_delta = -2;
+grounds[index(6, 4)]!.kinetic_energy_delta = -1;
+grounds[index(6, 7)]!.kinetic_energy_delta = -1;
+grounds[index(7, 4)]!.kinetic_energy_delta = -1;
+grounds[index(7, 6)]!.kinetic_energy_delta = -1;
+grounds[index(8, 2)]!.kinetic_energy_delta = -1;
+grounds[index(8, 7)]!.kinetic_energy_delta = -3;
 
 const static_blocks: StaticBlock[] = [
 	new StaticBlock({ position: Position.from([2, 7]), cor: Cor.from(0) }),
